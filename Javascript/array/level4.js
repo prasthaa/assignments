6a9c4925ea39b3af11ea940e3012e0d42cd9bca6
prasthaa <pratisbajra08@gmail.com>
2023-06-22 09:22:14 +0545

    duplicateobj

--- Javascript/array/level4.js
+++ Javascript/array/level4.js
@@ -1,10 +1,18 @@
 //Q1 find if hari is in allUsers array or not/
 //expected output is : 
 //true
-const name= 'hari'
-const allUsers = ['hari','shyam','thakur']
+const name = 'hari'
+const allUsers = ['hari', 'shyam', 'thakur']
 if (allUsers.includes('hari')) {
     console.log('The array contains Hari');
-  } else {
+} else {
     console.log('The array does not contain Hari');
-  }
+}
+
+let sum = 0
+allUsers.map((item, id) => {
+    if (item == 'hari') {
+        sum = sum + 1
+    }
+})
+console.log(sum) 
